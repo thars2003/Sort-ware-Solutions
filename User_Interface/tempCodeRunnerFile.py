2b873a3d6af921c5571bb5d8850c70ca3104454d
thars2003 <tharshinisubash@gmail.com>
2025-11-06 22:39:39 -0600

    removed all the weird temp files

--- User_Interface/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-# @app.route('/logo.png')
-# def logo():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'logo.png', mimetype='image/png')
