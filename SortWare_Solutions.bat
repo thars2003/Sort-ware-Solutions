b4ada1ebf3a16e2b439a61e2e384474b3b6d7418
thars2003 <tharshinisubash@gmail.com>
2025-11-08 00:21:41 -0600

    basic ui complete, still need to fix the emergency buttoon, fix colors as well

--- SortWare_Solutions.bat
+++ SortWare_Solutions.bat
@@ -4,7 +4,7 @@ cd /d "%PATH_DIR%User_Interface"
 
 
 start "Running Sort-ware Solutions" cmd /k python webapp.py
-timeout /t 1 >nul
+timeout /t 1 >nul  
 
 REM Open default browser
 start "Opening the website" http://127.0.0.1:5000
