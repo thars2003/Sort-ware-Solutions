7250fe05ca845f863bebe98430a14e1719061f7a
thars2003 <tharshinisubash@gmail.com>
2026-02-05 20:24:47 -0600

    multi-platform

--- Computer_Vision/poke_read_cards.py
+++ Computer_Vision/poke_read_cards.py
@@ -3,13 +3,23 @@ import pytesseract
 import re
 import os
 import numpy as np
-folder_path = '/Users/tharshinisubash/Documents/Sort-ware-Solutions/Scanned_Cards/'
+
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+SCANNED_DIR = os.path.join(BASE_DIR, "Scanned_Cards")
+
+img_path = os.path.join(SCANNED_DIR, "cam5.jpg")
+img = cv2.imread(img_path)
+
+if img is None:
+    raise FileNotFoundError(f"Could not load image: {img_path}")
+
+
 
 def read(i):
     filename = f"cam{i}.jpg"  # adjust extension if needed
-    img_path = os.path.join(folder_path, filename)
+    img_path = os.path.join(SCANNED_DIR, filename)
 
-    # 🔹 Load image
     img = cv2.imread(img_path)
     if img is None:
         print(f"Could not load {img_path}")
--- SortWare_Solutions.bat
+++ SortWare_Solutions.bat
@@ -1,11 +1,11 @@
-
 @echo off
 set "PATH_DIR=%~dp0"
-cd /d "%PATH_DIR%User_Interface"
+cd /d "%PATH_DIR%"
 
+echo Starting Sort-ware Solutions...
+start "Running Sort-ware Solutions" cmd /k C:\Python313\python.exe -m User_Interface.webapp
 
-start "Running Sort-ware Solutions" cmd /k python webapp.py
-timeout /t 1 >nul  
+timeout /t 2 >nul
 
 REM Open default browser
 start "Opening the website" http://127.0.0.1:5000
