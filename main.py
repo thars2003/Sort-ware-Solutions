8befda3f0ca01ebb349fb37bb9426006e0cc2603
thars2003 <tharshinisubash@gmail.com>
2026-02-24 17:31:57 -0600

    .

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import Computer_Vision.Magic as Magic
 import Computer_Vision.Pokemon as Pokemon
 import Computer_Vision.magic_read_cards as magic_read_cards
 
-text=magic_read_cards.read(f"cam6")
+text=magic_read_cards.read(f"cam7")
 print (text)
